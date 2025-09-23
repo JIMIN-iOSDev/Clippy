7c2b949f4f85c343d78c60d3dbbe9bc770d0ad4c
JIMIN-iOSDev <tig05015@naver.com>
2025-09-23 09:29:17 +0900

    Initial Commit

---
+++ Clippy/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clippy
+//
+//  Created by 서지민 on 9/23/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
