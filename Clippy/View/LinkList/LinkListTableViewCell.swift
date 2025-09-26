d034248f1f16894e2402f46dc1ba7c8d1bcb724e
Jimin Seo <tig05015@naver.com>
2025-09-26 23:11:23 +0900

    링크 리스트 화면 UI

--- Clippy/View/LinkList/LinkListTableViewCell.swift
+++ Clippy/View/LinkList/LinkListTableViewCell.swift
@@ -6,18 +6,122 @@
 //
 
 import UIKit
+import SnapKit
 
 class LinkListTableViewCell: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    
+    // MARK: - UI Components
+    private let thumbnailImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFill
+        imageView.layer.cornerRadius = 8
+        imageView.clipsToBounds = true
+        imageView.backgroundColor = .systemGray5
+        return imageView
+    }()
+    
+    let titleLabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 16, weight: .semibold)
+        return label
+    }()
+    
+    private let memoLabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14, weight: .regular)
+        label.text = "memo"
+        label.textColor = .secondaryLabel
+        return label
+    }()
+    
+    private let deadlineLabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        label.text = "deadline"
+        label.textColor = .systemRed
+        return label
+    }()
+    
+    private let urlLabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 12, weight: .regular)
+        label.text = "url"
+        label.textColor = .systemBlue
+        return label
+    }()
+    
+    private let ellipsisButton = {
+        let button = UIButton(type: .system)
+        let config = UIImage.SymbolConfiguration(pointSize: 18, weight: .regular)
+        button.setImage(UIImage(systemName: "ellipsis", withConfiguration: config), for: .normal)
+        button.tintColor = .black
+        return button
+    }()
+    
+    private let favoriteButton = {
+        let button = UIButton(type: .system)
+        let config = UIImage.SymbolConfiguration(pointSize: 20, weight: .medium)
+        button.setImage(UIImage(systemName: "star", withConfiguration: config), for: .normal)
+        button.setImage(UIImage(systemName: "star.fill", withConfiguration: config), for: .selected)
+        button.tintColor = .systemYellow
+        return button
+    }()
+    
+    override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+        super.init(style: style, reuseIdentifier: reuseIdentifier)
+        configureHierarchy()
+        configureLayout()
     }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
     }
-
+    
+    private func configureHierarchy() {
+        [thumbnailImageView, titleLabel, memoLabel, deadlineLabel, urlLabel, ellipsisButton, favoriteButton]
+            .forEach { contentView.addSubview($0) }
+    }
+    
+    private func configureLayout() {
+        thumbnailImageView.snp.makeConstraints { make in
+            make.verticalEdges.equalToSuperview().inset(12)
+            make.leading.equalToSuperview().offset(16)
+            make.size.equalTo(80)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(12)
+            make.leading.equalTo(thumbnailImageView.snp.trailing).offset(12)
+            make.trailing.equalTo(ellipsisButton.snp.leading).offset(-8)
+        }
+        
+        memoLabel.snp.makeConstraints { make in
+            make.top.equalTo(titleLabel.snp.bottom).offset(4)
+            make.leading.equalTo(titleLabel)
+        }
+        
+        deadlineLabel.snp.makeConstraints { make in
+            make.top.equalTo(memoLabel.snp.bottom).offset(4)
+            make.leading.equalTo(titleLabel)
+        }
+        
+        urlLabel.snp.makeConstraints { make in
+            make.top.equalTo(deadlineLabel.snp.bottom).offset(2)
+            make.leading.equalTo(titleLabel)
+            make.bottom.lessThanOrEqualToSuperview().offset(-12)
+        }
+        
+        ellipsisButton.snp.makeConstraints { make in
+            make.centerY.equalTo(titleLabel)
+            make.trailing.equalToSuperview().offset(-16)
+            make.size.equalTo(20)
+        }
+        
+        favoriteButton.snp.makeConstraints { make in
+            make.centerX.equalTo(ellipsisButton)
+            make.centerY.equalToSuperview()
+            make.size.equalTo(25)
+        }
+    }
 }
--- Clippy/View/LinkList/LinkListViewController.swift
+++ Clippy/View/LinkList/LinkListViewController.swift
@@ -6,24 +6,70 @@
 //
 
 import UIKit
+import SnapKit
+import RxSwift
+import RxCocoa
 
-class LinkListViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+final class LinkListViewController: BaseViewController {
+    // MARK: - Properties
+    private let disposeBag = DisposeBag()
+    var list = BehaviorRelay(value: ["rfkdls", "fdsfjsl", "fdsfsdl"])
+    
+    // MARK: - UI Components
+    private let tableView = {
+        let tv = UITableView()
+        tv.register(LinkListTableViewCell.self, forCellReuseIdentifier: LinkListTableViewCell.identifier)
+        tv.rowHeight = UITableView.automaticDimension
+        tv.estimatedRowHeight = 100
+        tv.backgroundColor = .clear
+        return tv
+    }()
+    
+    private let floatingAddButton = {
+        let button = UIButton(type: .system)
+        button.setImage(UIImage(systemName: "plus"), for: .normal)
+        button.tintColor = .white
+        button.backgroundColor = .systemBlue
+        button.layer.cornerRadius = 28
+        button.clipsToBounds = true
+        return button
+    }()
+    
+    // MARK: - Configuration
+    override func bind() {
+        list
+            .bind(to: tableView.rx.items(cellIdentifier: LinkListTableViewCell.identifier, cellType: LinkListTableViewCell.self)) { (row, item, cell) in
+                cell.titleLabel.text = item
+            }
+            .disposed(by: disposeBag)
+        
+        floatingAddButton.rx.tap
+            .asDriver()
+            .drive(with: self) { owner, _ in
+                let vc = UINavigationController(rootViewController: EditLinkViewController())
+                owner.present(vc, animated: true)
+            }
+            .disposed(by: disposeBag)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func configureHierarchy() {
+        view.addSubview(tableView)
+        view.addSubview(floatingAddButton)
     }
-    */
-
+    
+    override func configureLayout() {
+        tableView.snp.makeConstraints { make in
+            make.edges.equalTo(view.safeAreaLayoutGuide)
+        }
+        
+        floatingAddButton.snp.makeConstraints { make in
+            make.size.equalTo(56)
+            make.trailing.equalToSuperview().offset(-26)
+            make.bottom.equalTo(view.safeAreaLayoutGuide).offset(-38)
+        }
+    }
+    
+    override func configureView() {
+        super.configureView()
+    }
 }
