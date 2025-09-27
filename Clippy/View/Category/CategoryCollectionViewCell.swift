f318319f7147f3b73d196c6519ec5b92a5aca0cd
Jimin Seo <tig05015@naver.com>
2025-09-27 22:58:51 +0900

    카테고리 컬렉션뷰 셀

--- Clippy/View/Category/CategoryCollectionViewCell.swift
+++ Clippy/View/Category/CategoryCollectionViewCell.swift
@@ -6,7 +6,97 @@
 //
 
 import UIKit
+import SnapKit
 
-class CategoryCollectionViewCell: UICollectionViewCell {
+final class CategoryCollectionViewCell: UICollectionViewCell {
     
+    private let containerView = {
+        let view = UIView()
+        view.backgroundColor = .systemBackground
+        view.layer.cornerRadius = 16
+        view.layer.shadowColor = UIColor.label.cgColor
+        view.layer.shadowOffset = CGSize(width: 0, height: 1)
+        view.layer.shadowOpacity = 0.05
+        view.layer.shadowRadius = 3
+        return view
+    }()
+    
+    private let iconBackgroundView = {
+        let view = UIView()
+        view.layer.cornerRadius = 24
+        return view
+    }()
+    
+    private let iconImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let titleLabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 14, weight: .semibold)
+        label.textAlignment = .center
+        return label
+    }()
+    
+    private let countLabel = {
+        let label = UILabel()
+        label.font = UIFont.systemFont(ofSize: 12, weight: .medium)
+        label.textColor = .secondaryLabel
+        label.textAlignment = .center
+        return label
+    }()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        configureUI()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func configureUI() {
+        contentView.addSubview(containerView)
+        iconBackgroundView.addSubview(iconImageView)
+        [iconBackgroundView, titleLabel, countLabel].forEach { containerView.addSubview($0) }
+        
+        containerView.snp.makeConstraints { make in
+            make.edges.equalToSuperview()
+        }
+        
+        iconBackgroundView.snp.makeConstraints { make in
+            make.top.equalToSuperview().offset(16)
+            make.centerX.equalToSuperview()
+            make.size.equalTo(48)
+        }
+        
+        iconImageView.snp.makeConstraints { make in
+            make.center.equalToSuperview()
+            make.size.equalTo(20)
+        }
+        
+        titleLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(iconBackgroundView.snp.bottom).offset(12)
+        }
+        
+        countLabel.snp.makeConstraints { make in
+            make.centerX.equalToSuperview()
+            make.top.equalTo(titleLabel.snp.bottom).offset(4)
+            make.bottom.lessThanOrEqualToSuperview().offset(-16)
+        }
+    }
+    
+    func configure(with item: CategoryItem) {
+        titleLabel.text = item.title
+        countLabel.text = "\(item.count)개"
+        iconBackgroundView.backgroundColor = item.backgroundColor
+        
+        let config = UIImage.SymbolConfiguration(pointSize: 20, weight: .medium)
+        iconImageView.image = UIImage(systemName: item.iconName, withConfiguration: config)
+        iconImageView.tintColor = item.iconColor
+    }
 }
